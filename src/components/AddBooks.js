e214691746f438bbede5a2b11296c7e69c473838
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-08-05 21:45:27 -0500

    fisrt attempt to add books

--- src/components/AddBooks.js
+++ src/components/AddBooks.js
@@ -1,10 +1,22 @@
-import Book from './bookClass';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
+import progress from '../assets/progress.jpg';
 
-const AddBooks = (arr) => {
-  const author = document.querySelector('.author-value');
-  const title = document.querySelector('.title-value');
-  const newBook = new Book(title.value, author.value);
-  arr.push(newBook);
+const AddBooks = (title, author) => {
+  const books = useSelector((state) => state.books);
+  useDispatch(addBook(books));
+  return (
+    <li key={books.length} className="book-data">
+      <p className="book-title">{title}</p>
+      <p className="book-author">{author}</p>
+      <button className="delete-button" type="button">Delete</button>
+      <div className="progress">
+        <img src={progress} alt="progress icon" className="image" />
+        25% completed
+      </div>
+    </li>
+  );
 };
 
 export default AddBooks;
