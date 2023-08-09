6da9039055f653d99d096901a9871d9e8b8d00f4
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-08-08 22:06:30 -0500

    add books in dom

--- src/components/AddBooks.js
+++ src/components/AddBooks.js
@@ -1,22 +1,10 @@
-import { useSelector } from 'react-redux/es/hooks/useSelector';
-import { useDispatch } from 'react-redux';
-import { addBook } from '../redux/books/booksSlice';
-import progress from '../assets/progress.jpg';
+/* eslint-disable import/no-extraneous-dependencies */
+import { v4 as uuidv4 } from 'uuid'
 
-const AddBooks = (title, author) => {
-  const books = useSelector((state) => state.books);
-  useDispatch(addBook(books));
-  return (
-    <li key={books.length} className="book-data">
-      <p className="book-title">{title}</p>
-      <p className="book-author">{author}</p>
-      <button className="delete-button" type="button">Delete</button>
-      <div className="progress">
-        <img src={progress} alt="progress icon" className="image" />
-        25% completed 
-      </div>
-    </li>
-  );
-};
-
-export default AddBooks;
+const Addbooks = (title, author) => {
+  return {
+    title: title,
+    author: author,
+    id: uuidv4(),
+  }
+}
--- src/components/ListBook.js
+++ src/components/ListBook.js
@@ -1,5 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 const Booklist = () => {
-  
+  const [books, setBooks] = useState([
+    {
+      title: 'Books',
+      author: 'Kevin',
+      id: uuidv4(),
+    },
+    {
+      title: 'Boo',
+      author: 'Ke',
+      id: uuidv4(),
+    },
+  ]);
+  return (
+    <ul className="books-container">
+      {books.map((book) => (
+        <li className="book" key={book.id}>
+          <p className="title">{book.title}</p>
+          <p className="author">{book.author}</p>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Booklist;
