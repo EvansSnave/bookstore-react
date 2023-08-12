6f3444a172acfcf6d241e133a59ff75cba943e4c
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-08-12 13:08:47 -0500

    add axios

--- src/components/RenderBooks.js
+++ src/components/RenderBooks.js
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types';
+import {
+  removeBookFromServer,
+  removeBookFromList,
+  getBooksFromServer,
+} from '../redux/books/booksSlice';
+import { useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeBook } from '../redux/books/booksSlice';
 import AddNewBook from './AddBooks';
 import progress from '../assets/progress.jpg';
 
 function BookCard({
-  item_id, title, author, category,
+  item_id, title, author, category= 'none', handleRemoveBook,
 }) {
-  const dispatch = useDispatch();
-
   return (
     <article className="book-card">
       <div className="book-card__details">
@@ -23,7 +26,7 @@ function BookCard({
             <p>Comments</p>
           </li>
           <li className="remove-container">
-            <button type="button" onClick={() => { dispatch(removeBook(item_id)); }}>Remove</button>
+            <button type="button" onClick={() => handleRemoveBook(item_id)}>Remove</button>
           </li>
           <li className="action__item">
             <p>Edit</p>
@@ -54,31 +57,36 @@ BookCard.propTypes = {
   item_id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
-  category: PropTypes.string.isRequired,
+  category: PropTypes.string,
+  handleRemoveBook: PropTypes.func.isRequired,
 };
 
-function BookList() {
-  const bookList = useSelector((state) => state.booksArr.books);
-  const output = bookList.map((book) => (
-    <BookCard
-      key={uuidv4()}
-      item_id={book.item_id}
-      title={book.title}
-      author={book.author}
-      category={book.category}
-    />
-  ));
-
-  return output;
-}
-
 export default function BookCards() {
+  const dispatch = useDispatch();
+  const bookList = useSelector((state) => state.bookshelf.books);
+  const allBooks = Object.values(bookList);
+
+  const handleRemoveBook = (item_id) => {
+    dispatch(removeBookFromList(item_id));
+    dispatch(removeBookFromServer(item_id));
+  };
+
+  useEffect(() => {
+    dispatch(getBooksFromServer());
+  }, [dispatch]);
+
   return (
-    <>
-      <section className="books-container">
-        <BookList />
-      </section>
-      <AddNewBook />
-    </>
+    <section id="book-cards">
+      {allBooks.map((book) => (
+        <BookCard
+          key={book.item_id}
+          item_id={book.item_id}
+          title={book.title}
+          author={book.author}
+          handleRemoveBook={handleRemoveBook}
+        />
+      ))}
+      <AddNewBook></AddNewBook>
+    </section>
   );
 }
